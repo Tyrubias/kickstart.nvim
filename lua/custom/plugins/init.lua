889d689507d333b2d976e086d724a016ef7750f9
Victor Song <vsong1618@gmail.com>
2024-10-13 00:35:19 -0500

    Airline updates

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,12 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  { 'vim-airline/vim-airline-themes' },
+  {
+    'vim-airline/vim-airline',
+    config = function()
+      vim.g.airline_theme = 'base16_dracula'
+    end,
+  },
+}
